45cd064b7bf88d2868ae38c5f1d37a2079c8124d
NayreOliveira <nayreoliveira@hotmail.com>
2022-12-11 16:18:18 -0300

    new

--- src/App.js
+++ src/App.js
@@ -1,10 +1,51 @@
+import { ColorModeContext, useMode } from "./theme";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { Routes, Route, } from "react-router-dom";
+import Sidebar from "./scenes/global/Sidebar";
+import Dashboard  from "./scenes/dashboard";
+// import Team from "./scenes/team";
+// import Invoices from "./scenes/invoices";
+// import Contacts  from "./scenes/contacts";
+// import Bar from "./scenes/bar";
+// import Form from "./scenes/form";
+// import Line from "./scenes/line";
+// import Pie from ",/scenes/pie";
+// import FAQ from "./scenes/faq";
+// import Geography from "./scenes/geography";
+// import Calendar from "./scenes/calendar";
+
+
+
+
 
 
 function App() {
-  return (
-    <div className="app">
+  const [ theme, colorMode] = useMode();
 
-    </div>
+  return ( 
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+      <CssBaseline />
+    <div className="app">
+      <Sidebar />
+      <main className="content">
+    <Topbar />
+    <Routes>
+       <Route path="/" element={<Dashboard />} /> 
+       {/* <Route path="/team" element={<Team />} /> */}
+       {/* <Route path="/contacts" element={<Contacts />} /> */}
+       {/* <Route path="/invoices" element={<Invoices />} /> */}
+       {/* <Route path="/form" element={<Form />} /> */} 
+       {/* <Route path="/pie" element={<Pie />} />} */}
+       {/* <Route path="/line" element={<Line />} />} */}
+       {/* <Route path="/faq" element={<FAQ />} />} */}
+       {/* <Route path="/geography" element={<Geography />} />} */}
+       {/* <Route path="/calendar" element={<Calendar />} />} */}
+         </Routes> 
+      </main>
+      </div>
+     </ThemeProvider>
+   </ColorModeContext.Provider>
   );
 }
 
--- src/scenes/global/Sidebar.jsx
+++ src/scenes/global/Sidebar.jsx
@@ -1,4 +1,26 @@
+import { useState } from "react";
+import { ProSidebar, Menu, MenuItem } from "react-pro-sidebar";
+import { Box, IconButton, Typography, useTheme } from '@mui/material';
+import { Link } from "react-router-dom";
+import { tokens } from "../../theme";
+import HomeOutlinedIcon from "@mui/icons-material/HomeOutlined";
+import PeopleOutlinedIcon from "@mui/icons-material/PeopleOutlined";
+import ContactsOutlinedIcon from "@mui/icons-material/ContactsOutlined";
+import ReceiptOutlinedIcon from "@mui/icons-material/ReceiptOutlined";
+import PersonOutlinedIcon from "@mui/icons-material/PersonOutlined";
+import CalendarTodayOutlinedIcon from "@mui/icons-material/CalendarTodayOutlined";
+import HelpOutlinedIcon from "@mui/icons-material/HelpOutlined";
+import BarChartOutlinedIcon from "@mui/icons-material/BarChartOutlined";
+import PieChartOutlinedIcon from "@mui/icons-material/PieChartOutlined";
+import TimelineOutlinedIcon from "@mui/icons-material/TimelineOutlined";
+import MenuOutlinedIcon from "@mui/icons-material/MenuOutlined";
+import MapOutlinedIcon from "@mui/icons-material/MapOutlined";
+
+
+
 const Sidebar = () => {
+    const theme = useTheme();
+    const colors = tokens();
     return <div>Sidebar</div>
     
 }
